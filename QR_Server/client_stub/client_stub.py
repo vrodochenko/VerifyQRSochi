852ee7fdc81b3aeaeab37815dee081d8293f91cf
Игнат Мамедов <studentignat@gmail.com>
2019-09-28 23:54:22 +0300

    crypto_rsa

--- QR_Server/client_stub/client_stub.py
+++ QR_Server/client_stub/client_stub.py
@@ -1,5 +1,5 @@
 import requests
-fio = {"FIO":"Арабаджиян Артем Лукьянович", "Day_of_birth":"29.12.2012", "Ser_num":"6012 566778", "Start_of_tour":"12.12.2016", "Finish_of_tour":"31.12.2016"}
+fio = {"FIO":"Мамедов Игнат Эмильевич", "Day_of_birth":"28.08.1996", "Ser_num":"6342 445767", "Start_of_tour":"28.09.2019", "Finish_of_tour":"01.12.2019"}
 res = requests.post('http://10.178.195.95:5000/value', json=fio)
 if res.ok:
     print(res)
